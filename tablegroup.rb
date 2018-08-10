5ca6eb9c3449b2fd1d06228f59c2c3f423a8aeb1
renaupadhyay <renaupadhyay01@gmail.com>
2018-08-10 20:09:56 +0000

    automatically backed up by learn

--- tablegroup.rb
+++ tablegroup.rb
@@ -4,3 +4,11 @@ class Table
     @live= live
     @food = food
   end
+  
+  def base_traits
+    puts "It is human. It is female. It is in New York City. It codes."
+  end
+  
+  def favorite_color
+    @color 
+  end
