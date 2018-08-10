02eb20011cddfe83f2b88f5f98cc87ee224b744f
renaupadhyay <renaupadhyay01@gmail.com>
2018-08-10 20:19:42 +0000

    automatically backed up by learn

--- tablegroup.rb
+++ tablegroup.rb
@@ -27,4 +27,4 @@ erika= Table.new("blue", "Riverdale", "tacos")
 sandy= Table.new("purple", "Les", "dessert")
 rena= Table.new("pink", "Florida", "pizza")
   
-puts erika.where_they_live
+sandy.base_traits
