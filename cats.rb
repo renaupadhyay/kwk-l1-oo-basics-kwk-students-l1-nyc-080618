d046ff0b6b6ae9481cae029777aff4ed5ad9834b
renaupadhyay <renaupadhyay01@gmail.com>
2018-08-10 19:32:36 +0000

    automatically backed up by learn

--- cats.rb
+++ cats.rb
@@ -5,5 +5,8 @@ class Cat
     @cute =cute 
   end
 end
+
+tiger= Cat.new ("super cute", "super fuzzy"), "very mean")
+garfield= Cat.new ("ewwwww", "yeah...kind", "well he hates mondays")
   
-  
+  puts Cat
